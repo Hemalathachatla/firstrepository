fa9763e082723b3ab98b25b734b4f8ffc36d445b
Lenovo <Lenovo@Sreeki-PC>
2024-06-11 11:03:24 -0400

    addition

---
+++ gitproject/src/git/addition.java
@@ -0,0 +1,11 @@
+package git;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10, b =20;
+System.out.println("addition of two numbers" +(a+b));
+	}
+
+}
